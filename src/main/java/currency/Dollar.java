595382a2287d637f921ad8aa6677c6e8e1bebd07
lukasz_czubernat <lukasz_czubernat@epam.com>
2016-06-23 11:54:27 +0200

    Encaplusate dollar and refactor test to comparing objects insted of their state

--- src/main/java/currency/Dollar.java
+++ src/main/java/currency/Dollar.java
@@ -4,7 +4,7 @@ package currency;
  * Created by lucz on 23.06.16.
  */
 public class Dollar {
-    public int amount;
+    private int amount;
 
     public Dollar(int amount) {
         this.amount = amount;
