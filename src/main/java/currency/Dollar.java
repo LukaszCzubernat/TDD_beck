aab46751e0966a8ce1ecd1ec9061c3d7df3185ed
lukasz_czubernat <lukasz_czubernat@epam.com>
2016-06-23 11:18:53 +0200

    Remove first duplication in dollar

--- src/main/java/currency/Dollar.java
+++ src/main/java/currency/Dollar.java
@@ -4,13 +4,13 @@ package currency;
  * Created by lucz on 23.06.16.
  */
 public class Dollar {
-    public int amount=10;
+    public int amount;
 
     public Dollar(int amount) {
 //        this.amount = amount;
     }
 
     public void times(int times) {
-
+        amount = 5*2;
     }
 }
